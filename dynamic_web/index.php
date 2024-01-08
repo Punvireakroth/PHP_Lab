8330e1bce497bf0b36cbe7a1fc413b79544267cc
Pun VireakRoth <vireakrothpun@gmail.com>
2024-01-08 23:43:39 +0300

    refactor

--- dynamic_web/index.php
+++ dynamic_web/index.php
@@ -5,10 +5,12 @@ require "function.php";
 require "Database.php";
 
 
+$config = require "config.php";
 
-$db = new Database();
 
-$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
+$db = new Database($config['database']);
+
+$posts = $db->query("select * from posts")->fetchAll();
 
 
 
