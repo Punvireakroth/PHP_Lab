a7ed3103dd0dd963a6e593da34fa30df6d88e730
Pun VireakRoth <vireakrothpun@gmail.com>
2024-01-10 01:44:05 +0300

    Refactor methods ownership

--- note/controllers/note.php
+++ note/controllers/note.php
@@ -12,16 +12,12 @@ $currentUserId = 1;
 
 $note = $db->query("select * from notes where id = :id", [
     "id" => $_GET["id"]
-])->fetch();
-
-if (!$note) {
-    abort();
-}
+])->findOrFail();
 
 
-if ($note['user_id'] !== $currentUserId) {
-    abort(Response::FORBIDDEN);
-}
+authorize($note['user_id'] === $currentUserId);
+
+
 
 
 require 'views/note.view.php';
--- note/function.php
+++ note/function.php
@@ -9,6 +9,13 @@ function dd($value)
     die();
 }
 
+function authorize($condition, $status = Response::FORBIDDEN)
+{
+    if (!$condition) {
+        abort($status);
+    }
+}
+
 function urlIs($value)
 {
     return $_SERVER['REQUEST_URI'] === $value;
