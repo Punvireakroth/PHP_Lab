01d1edd01e8cc11a05f77fa41134adf8f2feadec
Pun VireakRoth <vireakrothpun@gmail.com>
2024-01-10 06:21:00 +0300

    Authorization check

--- note/index.php
+++ note/index.php
@@ -3,6 +3,7 @@
 
 require "function.php";
 require "Database.php";
+require "Response.php";
 require "router.php";
 
 
---
+++ note/views/403.php
@@ -0,0 +1,11 @@
+<?php require("partials/head.php") ?>
+<?php require("partials/nav.php") ?>
+
+
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <h1 class="text-2xl font-bold">403: You are not authorize to access this page.</h1>
+        <p class="mt-4"><a class="text-blue underline" href="/">Go back home.</a></p>
+    </div>
+</main>
+<?php require("partials/footer.php") ?>;
