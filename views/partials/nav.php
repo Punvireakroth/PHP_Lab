247f07d5d86b7d78ef6b963cffa2545c06ec792d
Pun VireakRoth <vireakrothpun@gmail.com>
2024-01-21 00:54:04 +0300

    Login page I

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -40,13 +40,16 @@
                     <div class="relative ml-3">
                         <div>
                             <?php if ($_SESSION['user'] ?? false): ?>
-                                        <button type="button" class="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                                            <span class="sr-only">Open user menu</span>
+                                                                                                                                                                                                                                                                        <button type="button" class="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                                                                                                                                                                                                                                                                            <span class="sr-only">Open user menu</span>
 
-                                            <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-                                        </button>
+                                                                                                                                                                                                                                                                            <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                                                                                                                                                                                                                                                                        </button>
                             <?php else: ?>
-                                        <a href="/register" class="text-white">Register</a>
+                                                                                                                                                                                                                                        <a href="/register"
+                                                                                                                                                                                                                                            class="<?= urlIs('/register') ? "bg-gray-900 text-white" : "text-gray-300" ?>  text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Register</a>
+                                                                                                                                                                                                                                        <a href="/login"
+                                                                                                                                                                                                                                            class="<?= urlIs('/login') ? "bg-gray-900 text-white" : "text-gray-300" ?>  text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Login</a>
                             <?php endif; ?>
                         </div>
 
